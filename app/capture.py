c27b942716f1447fd22bf3dd46d3e18075dba0d9
Nhật Linh Nguyễn <datnatlin57@gmail.com>
2024-03-02 17:24:20 +0300

    third commit

--- app/capture.py
+++ app/capture.py
@@ -5,25 +5,18 @@ cam = cv.VideoCapture(0)
 def gen_frame():
     while True:
         ret, frame = cam.read()
+        frame = cv.flip(frame, 1)
         if not ret:
             break
-
-        ret, buffer = cv.imencode('.jpg', frame)
-        frame_bytes = buffer.tobytes()
-
-        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
-
-        #cv.imshow("Photo", cv.flip(image, 1))
+        
+        # Extract the encoded image data from the tuple
+        _, buffer = cv.imencode('.jpg', frame)  # Use unpacking to avoid naming the first element
+        
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+        
+        if cv.waitKey(1) & 0xff == ord(' '):
+            cv.imwrite("Photo.png", frame)
+            break
 
     cam.release()
-
-cam.release()
-cv.destroyAllWindows()
-'''
-        yield (b'--frame\r\n'
-           b'Content-Type: image/jpeg\r\n\r\n' + cv.imencode('.jpg', image)[1].tobytes() + b'\r\n')
-
-        if cv.waitKey(1) & 0xff == ord(' '):
-            cv.imwrite("Photo.png", cv.flip(image, 1))
-            break
-        '''
